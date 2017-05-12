de4cd2faa22e0251897251b202ab244beb64e5ea
Trevis Thomas <trevisthomas@gmail.com>
2017-05-12 12:22:36 -0500

    Hot fix ++version

--- GitExperiment/ViewController.swift
+++ GitExperiment/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("I am alive.")
+        print("I am alive. Hot-fixed.")
     }
 
     override func didReceiveMemoryWarning() {
